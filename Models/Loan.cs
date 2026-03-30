93e458ce1965845077327994c70daff90d7abfdb
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-03-30 21:11:36 +0200

    Dodano klasę Loan

---
+++ Models/Loan.cs
@@ -0,0 +1,12 @@
+﻿public class Loan
+{
+    public int Id { get; set; }
+    public Equipment Equipment { get; set; }
+    public User  User { get; set; }
+    
+    public DateTime LoanDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    
+    public decimal Penalty { get; set; }
+}
