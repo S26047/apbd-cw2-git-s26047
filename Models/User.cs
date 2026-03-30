93a6371573dbd7b1c09017909ebae3c2a027f113
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-03-30 21:07:15 +0200

    Utworzono klasę abstrakcyjną User

---
+++ Models/User.cs
@@ -0,0 +1,7 @@
+﻿public abstract class User
+{
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxLoans {get; }
+}
